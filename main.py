c21ada6433682853bea6089b72dcd85b45e384b5
Vedang Naik <vedang.a.naik@gmail.com>
2021-09-06 01:09:12 +0530

    Added coordinates to printout + some renaming

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from colorama import Back, init, Style, Fore
 from enum import Enum
 
+from colorama.initialise import reset_all
+
 class Cube:
     __colorMapping = {
         'r': Back.RED,
@@ -15,8 +17,21 @@ class Cube:
         F = 1
         F_ = 2
         F2 = 3
-
         R = 4
+        R_ = 5
+        R2 = 6
+        B = 7
+        B_ = 8
+        B2 = 9
+        L = 10
+        L_ = 11
+        L2 = 12
+        U = 13
+        U_ = 14
+        U2 = 15
+        D = 16
+        D_ = 17
+        D2 = 18
 
     class __Direction(Enum):
         Normal = 1
@@ -28,8 +43,8 @@ class Cube:
     __RIGHT = 1
     __BACK = 2
     __LEFT = 3
-    __TOP = 4
-    __BOTTOM = 5
+    __UP = 4
+    __DOWN = 5
 
     def __rotateFace(self, face, dir):
         if dir == Cube.__Direction.Normal:
@@ -45,47 +60,48 @@ class Cube:
         if move == Cube.Move.F:
             self.__rotateFace(self.__cubeRepr[Cube.__FRONT], Cube.__Direction.Normal)
             # Rotate 
-            self.__cubeRepr[Cube.__LEFT][2][2], self.__cubeRepr[Cube.__TOP][2][0], self.__cubeRepr[Cube.__RIGHT][0][0], self.__cubeRepr[Cube.__BOTTOM][0][2] = self.__cubeRepr[Cube.__BOTTOM][0][2], self.__cubeRepr[Cube.__LEFT][2][2], self.__cubeRepr[Cube.__TOP][2][0], self.__cubeRepr[Cube.__RIGHT][0][0]
-            self.__cubeRepr[Cube.__LEFT][1][2], self.__cubeRepr[Cube.__TOP][2][1], self.__cubeRepr[Cube.__RIGHT][1][0], self.__cubeRepr[Cube.__BOTTOM][0][1] = self.__cubeRepr[Cube.__BOTTOM][0][1], self.__cubeRepr[Cube.__LEFT][1][2], self.__cubeRepr[Cube.__TOP][2][1], self.__cubeRepr[Cube.__RIGHT][1][0]
-            self.__cubeRepr[Cube.__LEFT][0][2], self.__cubeRepr[Cube.__TOP][2][2], self.__cubeRepr[Cube.__RIGHT][2][0], self.__cubeRepr[Cube.__BOTTOM][0][0] = self.__cubeRepr[Cube.__BOTTOM][0][0], self.__cubeRepr[Cube.__LEFT][0][2], self.__cubeRepr[Cube.__TOP][2][2], self.__cubeRepr[Cube.__RIGHT][2][0]
+            self.__cubeRepr[Cube.__LEFT][2][2], self.__cubeRepr[Cube.__UP][2][0], self.__cubeRepr[Cube.__RIGHT][0][0], self.__cubeRepr[Cube.__DOWN][0][2] = self.__cubeRepr[Cube.__DOWN][0][2], self.__cubeRepr[Cube.__LEFT][2][2], self.__cubeRepr[Cube.__UP][2][0], self.__cubeRepr[Cube.__RIGHT][0][0]
+            self.__cubeRepr[Cube.__LEFT][1][2], self.__cubeRepr[Cube.__UP][2][1], self.__cubeRepr[Cube.__RIGHT][1][0], self.__cubeRepr[Cube.__DOWN][0][1] = self.__cubeRepr[Cube.__DOWN][0][1], self.__cubeRepr[Cube.__LEFT][1][2], self.__cubeRepr[Cube.__UP][2][1], self.__cubeRepr[Cube.__RIGHT][1][0]
+            self.__cubeRepr[Cube.__LEFT][0][2], self.__cubeRepr[Cube.__UP][2][2], self.__cubeRepr[Cube.__RIGHT][2][0], self.__cubeRepr[Cube.__DOWN][0][0] = self.__cubeRepr[Cube.__DOWN][0][0], self.__cubeRepr[Cube.__LEFT][0][2], self.__cubeRepr[Cube.__UP][2][2], self.__cubeRepr[Cube.__RIGHT][2][0]
+
         elif move == Cube.Move.R:
             self.__rotateFace(self.__cubeRepr[Cube.__RIGHT], Cube.__Direction.Normal)
-
-            self.__cubeRepr[Cube.__FRONT][2][2], self.__cubeRepr[Cube.__TOP][2][2], self.__cubeRepr[Cube.__BACK][0][0], self.__cubeRepr[Cube.__BOTTOM][2][2] = self.__cubeRepr[Cube.__BOTTOM][2][2], self.__cubeRepr[Cube.__FRONT][2][2], self.__cubeRepr[Cube.__TOP][2][2], self.__cubeRepr[Cube.__BACK][0][0]
-            self.__cubeRepr[Cube.__FRONT][1][2], self.__cubeRepr[Cube.__TOP][1][2], self.__cubeRepr[Cube.__BACK][1][0], self.__cubeRepr[Cube.__BOTTOM][1][2] = self.__cubeRepr[Cube.__BOTTOM][1][2], self.__cubeRepr[Cube.__FRONT][1][2], self.__cubeRepr[Cube.__TOP][1][2], self.__cubeRepr[Cube.__BACK][1][0]
-            self.__cubeRepr[Cube.__FRONT][0][2], self.__cubeRepr[Cube.__TOP][0][2], self.__cubeRepr[Cube.__BACK][2][0], self.__cubeRepr[Cube.__BOTTOM][0][2] = self.__cubeRepr[Cube.__BOTTOM][0][2], self.__cubeRepr[Cube.__FRONT][0][2], self.__cubeRepr[Cube.__TOP][0][2], self.__cubeRepr[Cube.__BACK][2][0]
+            self.__cubeRepr[Cube.__FRONT][2][2], self.__cubeRepr[Cube.__UP][2][2], self.__cubeRepr[Cube.__BACK][0][0], self.__cubeRepr[Cube.__DOWN][2][2] = self.__cubeRepr[Cube.__DOWN][2][2], self.__cubeRepr[Cube.__FRONT][2][2], self.__cubeRepr[Cube.__UP][2][2], self.__cubeRepr[Cube.__BACK][0][0]
+            self.__cubeRepr[Cube.__FRONT][1][2], self.__cubeRepr[Cube.__UP][1][2], self.__cubeRepr[Cube.__BACK][1][0], self.__cubeRepr[Cube.__DOWN][1][2] = self.__cubeRepr[Cube.__DOWN][1][2], self.__cubeRepr[Cube.__FRONT][1][2], self.__cubeRepr[Cube.__UP][1][2], self.__cubeRepr[Cube.__BACK][1][0]
+            self.__cubeRepr[Cube.__FRONT][0][2], self.__cubeRepr[Cube.__UP][0][2], self.__cubeRepr[Cube.__BACK][2][0], self.__cubeRepr[Cube.__DOWN][0][2] = self.__cubeRepr[Cube.__DOWN][0][2], self.__cubeRepr[Cube.__FRONT][0][2], self.__cubeRepr[Cube.__UP][0][2], self.__cubeRepr[Cube.__BACK][2][0]
 
     def draw(self):
-        # Top Padding
-        print('\n' * 2)
+        print()
 
         # Full U face.
-        for row in self.__cubeRepr[4]:
+        for row in range(3):
             print(13 * " ", end="")
-            for cell in row:
-                print(f" {self.__colorMapping[cell]}  ", end="")
+            for col in range(3):
+                cell = self.__cubeRepr[Cube.__UP][row][col]
+                print(f" {Fore.BLACK}{self.__colorMapping[cell]}{row}{col}", end="")
             print(" ")
         print()
 
         # First row of L, F, R, B faces
-        for row in [0, 1, 2]:
+        for row in range(3):
             print("| ", end="")
-            for face in [3, 0, 1, 2]:
-                for cell in self.__cubeRepr[face][row]:
-                    print(f" {self.__colorMapping[cell]}  ", end="")
+            for face in [Cube.__LEFT, Cube.__FRONT, Cube.__RIGHT, Cube.__BACK]:
+                for col in range(3):
+                    cell = self.__cubeRepr[face][row][col]
+                    print(f" {self.__colorMapping[cell]}{row}{col}", end="")
                 print(" |", end="")
             print()
         print()
 
         # Full D face.
-        for row in self.__cubeRepr[5]:
+        for row in range(3):
             print(13 * " ", end="")
-            for cell in row:
-                print(f" {self.__colorMapping[cell]}  ", end="")
+            for col in range(3):
+                cell = self.__cubeRepr[Cube.__DOWN][row][col]
+                print(f" {Fore.BLACK}{self.__colorMapping[cell]}{row}{col}", end="")
             print(" ")
-
-        # Bottom Padding
-        print('\n' * 2)
+        print()
+        print()
 
     def __init__(self):
         self.__cubeRepr = [
@@ -128,6 +144,6 @@ if __name__ == "__main__":
 
     a = Cube()
     a.draw()
-    a.turn(Cube.Move.F)
-    a.turn(Cube.Move.R)
+    # a.turn(Cube.Move.F)
+    # a.turn(Cube.Move.R)
     a.draw()
