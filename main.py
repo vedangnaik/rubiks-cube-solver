2f2ff6224ab2352921b8314f808b3e338441f5b7
Vedang Naik <vedang.a.naik@gmail.com>
2021-09-06 01:36:03 +0530

    Added cube flips to utilize symmetry to reuse code for L, B, etc.

--- main.py
+++ main.py
@@ -38,6 +38,10 @@ class Cube:
         Prime = 2
         OneEighty = 3
 
+    class __Axis(Enum):
+        UP_DOWN = 1
+        RIGHT_LEFT = 2
+        FRONT_BACK = 3
     # Face numbers. An enum is too inconvenient.
     __FRONT = 0
     __RIGHT = 1
@@ -45,6 +49,22 @@ class Cube:
     __LEFT = 3
     __UP = 4
     __DOWN = 5
+
+
+    def __flipCubeAcrossAxis(self, axis):
+        if axis == Cube.__Axis.UP_DOWN:
+            # First, swap FRONT-BACK and RIGHT-LEFT
+            self.__cubeRepr[Cube.__FRONT], self.__cubeRepr[Cube.__BACK] = self.__cubeRepr[Cube.__BACK], self.__cubeRepr[Cube.__FRONT]
+            self.__cubeRepr[Cube.__RIGHT], self.__cubeRepr[Cube.__LEFT] = self.__cubeRepr[Cube.__LEFT], self.__cubeRepr[Cube.__RIGHT]
+            # Then, 180 rotate UP and DOWN.
+            self.__rotateFace(self.__cubeRepr[Cube.__UP], Cube.__Direction.OneEighty)
+            self.__rotateFace(self.__cubeRepr[Cube.__DOWN], Cube.__Direction.OneEighty)
+
+        elif axis == Cube.__Axis.RIGHT_LEFT:
+            pass
+        elif axis == Cube.__Axis.FRONT_BACK:
+            pass
+
 
     def __rotateFace(self, face, dir):
         if dir == Cube.__Direction.Normal:
@@ -54,7 +74,12 @@ class Cube:
         elif dir == Cube.__Direction.Prime:
             pass
         elif dir == Cube.__Direction.OneEighty:
-            pass
+            # Flip opposite corners.
+            face[0][0], face[2][2] = face[2][2], face[0][0]
+            face[0][2], face[2][0] = face[2][0], face[0][2]
+            # Flip opposite edges.
+            face[1][0], face[1][2] = face[1][2], face[1][0]
+            face[0][1], face[2][1] = face[2][1], face[0][1]
 
     def turn(self, move):
         if move == Cube.Move.F:
@@ -69,6 +94,16 @@ class Cube:
             self.__cubeRepr[Cube.__FRONT][2][2], self.__cubeRepr[Cube.__UP][2][2], self.__cubeRepr[Cube.__BACK][0][0], self.__cubeRepr[Cube.__DOWN][2][2] = self.__cubeRepr[Cube.__DOWN][2][2], self.__cubeRepr[Cube.__FRONT][2][2], self.__cubeRepr[Cube.__UP][2][2], self.__cubeRepr[Cube.__BACK][0][0]
             self.__cubeRepr[Cube.__FRONT][1][2], self.__cubeRepr[Cube.__UP][1][2], self.__cubeRepr[Cube.__BACK][1][0], self.__cubeRepr[Cube.__DOWN][1][2] = self.__cubeRepr[Cube.__DOWN][1][2], self.__cubeRepr[Cube.__FRONT][1][2], self.__cubeRepr[Cube.__UP][1][2], self.__cubeRepr[Cube.__BACK][1][0]
             self.__cubeRepr[Cube.__FRONT][0][2], self.__cubeRepr[Cube.__UP][0][2], self.__cubeRepr[Cube.__BACK][2][0], self.__cubeRepr[Cube.__DOWN][0][2] = self.__cubeRepr[Cube.__DOWN][0][2], self.__cubeRepr[Cube.__FRONT][0][2], self.__cubeRepr[Cube.__UP][0][2], self.__cubeRepr[Cube.__BACK][2][0]
+
+        # Take advantage of RIGHT-LEFT symmetry to reuse the R code.
+        elif move == Cube.Move.L:
+            # Rotate the cube 180 degrees around the UP-DOWN axis.
+            self.__flipCubeAcrossAxis(Cube.__Axis.UP_DOWN)
+            # Now, do the normal right rotation. This is just one level of recursion.
+            self.turn(Cube.Move.R)
+            # Undo the flip by flipping again
+            self.__flipCubeAcrossAxis(Cube.__Axis.UP_DOWN)
+
 
     def draw(self):
         print()
