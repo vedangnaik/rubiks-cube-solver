a62250263a2b77d91058d28270ff166a7838c587
Vedang Naik <vedang.a.naik@gmail.com>
2021-09-06 19:12:39 +0530

    Save-commit for full 24-move list of white cross

--- main.py
+++ main.py
@@ -262,9 +262,35 @@ class Cube:
 
 
 
+
 whiteCrossMoves = [
+    # Bottom layer edges.
     ((Cube.FRONT, 2, 1), (Cube.DOWN, 0, 1), []),
-    ((Cube.DOWN, 0, 1), (Cube.FRONT, 2, 1), [Cube.Move.F_, Cube.Move.D, Cube.Move.R_, Cube.Move.D_])
+    ((Cube.DOWN, 0, 1), (Cube.FRONT, 2, 1), [Cube.Move.F_, Cube.Move.D, Cube.Move.R_, Cube.Move.D_]),
+    ((Cube.RIGHT, 2, 1), (Cube.DOWN, 1, 2), [Cube.Move.R2, Cube.Move.U, Cube.Move.F2]),
+    ((Cube.DOWN, 1, 2), (Cube.RIGHT, 2, 1), [Cube.Move.R, Cube.Move.F]),
+    ((Cube.BACK, 2, 1), (Cube.DOWN, 2, 1), [Cube.Move.B2, Cube.Move.U2, Cube.Move.F2]),
+    ((Cube.DOWN, 2, 1), (Cube.BACK, 2, 1), [Cube.Move.B, Cube.Move.R2, Cube.Move.F, Cube.Move.R2]),
+    ((Cube.LEFT, 2, 1), (Cube.DOWN, 1, 0), [Cube.Move.L2, Cube.Move.U_, Cube.Move.F2]),
+    ((Cube.DOWN, 1, 0), (Cube.LEFT, 2, 1), [Cube.Move.L, Cube.Move.F_]),
+    # Middle layer edges.
+    ((Cube.FRONT, 1, 2), (Cube.RIGHT, 1, 0), [Cube.Move.F]),
+    ((Cube.FRONT, 1, 2), (Cube.RIGHT, 1, 0), [Cube.Move.R, Cube.Move.U, Cube.Move.R_, Cube.Move.F2]),
+    ((Cube.LEFT, 1, 2), (Cube.BACK, 1, 0), [Cube.Move.R_, Cube.Move.U, Cube.Move.R, Cube.Move.F2]),
+    ((Cube.BACK, 1, 0), (Cube.LEFT, 1, 2), [Cube.Move.R2, Cube.Move.F, Cube.Move.R2]),
+    ((Cube.BACK, 1, 2), (Cube.LEFT, 1, 0), [Cube.Move.B_, Cube.Move.U2, Cube.Move.B, Cube.Move.F2]),
+    ((Cube.LEFT, 1, 0), (Cube.BACK, 1, 2), [Cube.Move.L, Cube.Move.U_, Cube.Move.L_, Cube.Move.F2]),
+    ((Cube.LEFT, 1, 2), (Cube.FRONT, 1, 0), [Cube.Move.L_, Cube.Move.U_, Cube.Move.L, Cube.Move.F2]),
+    ((Cube.FRONT, 1, 0), (Cube.LEFT, 1, 2), [Cube.Move.F_]),
+    # Top edges.
+    ((Cube.FRONT, 0, 1), (Cube.UP, 2, 1), [Cube.Move.F2]),
+    ((Cube.UP, 2, 1), (Cube.FRONT, 0, 1), [Cube.Move.U_, Cube.Move.R_, Cube.Move.F, Cube.Move.R]),
+    ((Cube.RIGHT, 0, 1), (Cube.UP, 1, 2), [Cube.Move.U, Cube.Move.F2]),
+    ((Cube.UP, 1, 2), (Cube.RIGHT, 0, 1), [Cube.Move.R_, Cube.Move.F, Cube.Move.R]),
+    ((Cube.BACK, 0, 1), (Cube.UP, 0, 1), [Cube.Move.U2, Cube.Move.F2]),
+    ((Cube.UP, 0, 1), (Cube.BACK, 0, 1), [Cube.Move.U, Cube.Move.R_, Cube.Move.F, Cube.Move.R]),
+    ((Cube.LEFT, 0, 1), (Cube.UP, 1, 0), [Cube.Move.U_, Cube.Move.F2]),
+    ((Cube.UP, 1, 0), (Cube.LEFT, 0, 1), [Cube.Move.L, Cube.Move.F_, Cube.Move.L_]),
 ]
 
 
@@ -278,7 +304,7 @@ if __name__ == "__main__":
     a.draw()
 
     print("Scrambling:")    
-    moves = [Cube.Move.D, Cube.Move.R, Cube.Move.D_, Cube.Move.F]
+    moves = [Cube.Move.D2]#, Cube.Move.R, Cube.Move.F, Cube.Move.D]
     for move in moves:
         a.turn(move)
     a.draw()
